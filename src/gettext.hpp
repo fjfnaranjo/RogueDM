87d1cb67b53e79c425e86ae4187541d48d73a61a
Francisco José Fernández Naranjo <fjfnaranjo@gmail.com>
2018-01-21 05:14:49 +0100

    Move all gettext definitions to a separate file

---
+++ src/gettext.hpp
@@ -0,0 +1,31 @@
+// Copyright 2018 Fco. Jose Fdez. Naranjo AKA github.com/fjfnaranjo
+
+// This file is part of RogueDM.
+//
+// RogueDM is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// RogueDM is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with RogueDM.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * \file gettext.hpp
+ * \brief File containing the gettext alias.
+ */
+
+#ifndef GETTEXT_HPP
+#define GETTEXT_HPP
+
+// Standard includes.
+#include <locale>
+
+#define _ gettext
+
+#endif // GETTEXT_HPP
