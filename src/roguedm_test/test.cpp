bf382016ea7e223d87f09269a72eb81afe274d8b
Francisco José Fernández Naranjo <fjfnaranjo@gmail.com>
2018-12-02 04:17:44 +0100

    Support for Google Test testing framework

--- src/roguedm_test/test.cpp
+++ src/roguedm_test/test.cpp
@@ -1,1 +1,14 @@
-int main(int argc, char *argv[]) {}
+#include "gtest/gtest.h"
+
+#include "../roguedm/InitialState.hpp"
+
+TEST(testMath, myCubeTest) {
+  auto initial = new roguedm::InitialState();
+  EXPECT_EQ(1000, 1000);
+  delete initial;
+}
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
