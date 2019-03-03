c012dfbbcdf1e3d67b43ee2bef7e6e74bb1c3c83
Francisco José Fernández Naranjo <fjfnaranjo@gmail.com>
2019-03-03 15:11:49 +0100

    Missing value initialization

--- src/roguedm/sdl2/CommandComposer.cpp
+++ src/roguedm/sdl2/CommandComposer.cpp
@@ -29,6 +29,8 @@ CommandComposer::CommandComposer () {
   emptyWord.content = RDM_WEMPTY;
   emptyWord.kind = RDM_WCLASS_NORMAL;
 
+  sentence = roguedm::Sentence({emptyWord});
+
 }
 
 CommandComposer::~CommandComposer () {}
